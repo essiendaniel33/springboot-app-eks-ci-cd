972bb2fff6aa15ab07eabff3641cdbd21dea85e8
essiendaniel33 <122430463+essiendaniel33@users.noreply.github.com>
2024-08-10 21:47:49 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "essien-bucket-app"
+    bucket         = "stero-s3-bucket"
     key            = "terraform_statefile"
     region         = "us-east-1"
     dynamodb_table = "essien-app-dynamodb"
