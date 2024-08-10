91fb711235e38333a9ea5b870ca8c9b3625acebe
essiendaniel33 <122430463+essiendaniel33@users.noreply.github.com>
2024-08-10 21:50:13 +0200

    Update loadbalancer.tf

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "main" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
-  port              = 80
+  port              = 8081
   protocol          = "HTTP"
 
   default_action {
