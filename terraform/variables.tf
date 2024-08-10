9957dde6a9b4e4a910ac89318f06462551643eab
essiendaniel33 <122430463+essiendaniel33@users.noreply.github.com>
2024-08-10 21:03:37 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,41 @@
-variable "region" {
-  description = "The AWS region where resources will be created"
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_id" {
-  description = "The ID of the VPC where resources will be created"
-  default     = "vpc-0a92a62ab4980f921"
+  description = "The ID of the VPC where the ECS cluster will be deployed"    
+  type        = string
+  default     = "vpc-08a20b564a94cad98"
 }
 
-variable "instance_ami" {
-  description = "AMI ID for the instances"
-  default     = "ami-0195204d5dce06d99"
+variable "subnet_ids" {
+  description = "A list of subnet IDs where the ECS services will be deployed"
+  type        = list(string)
+  default     = ["subnet-0b355c1e081bfb317","subnet-04e7a2e028dcb65ea"]       
 }
 
-variable "instance_key_name" {
-  description = "An Existing Keypair to be used for the instances"
-  default     = "essien-key"
+variable "cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  default     = "test-ecs-cluster"
 }
 
-variable "instance_subnet_id" {
-  description = "Public Subnet ID for the instances"
-  default     = "subnet-02fb7f22d0c732bdd"
+variable "service_name" {
+  description = "The name of the ECS service"
+  type        = string
+  default     = "test-ecs-service"
 }
 
-variable "instance_type" {
-  description = "The Instance type"
-  default     = "t2.medium"
+variable "container_port" {
+  description = "The port on which the container will listen"
+  type        = number
+  default     = 80
 }
 
-variable "s3_bucket_name" {
-  description = "The name of the S3 bucket for Terraform state"
-  default     = "essien-bucket-app"
-}
-
-variable "dynamodb_table_name" {
-  description = "The name of the DynamoDB table for Terraform state locking"
-  default     = "essien-app-dynamodb"
+variable "desired_count" {
+  description = "The number of desired tasks for the ECS service"
+  type        = number
+  default     = 3
 }
