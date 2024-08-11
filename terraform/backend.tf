75ce1a0055cd3445b82871b240a3c5b7fa8b842f
essiendaniel33 <122430463+essiendaniel33@users.noreply.github.com>
2024-08-11 14:57:55 +0200

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "stero-s3-bucket"
-    key            = "terraform_statefile"
-    region         = "us-east-1"
-    dynamodb_table = "essien-app-dynamodb"
-  }
-}
--- terraform/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "ecs_log_group" {
-  name              = "/ecs/springboot-app"        
-  retention_in_days = 30
-}
